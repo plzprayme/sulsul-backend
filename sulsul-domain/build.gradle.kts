8484d5557d036550675a6449e73b5e898e364a58
plzprayme <dbfpzk142@gmail.com>
2023-05-27 19:55:35 +0900

    WIP MONGO

--- sulsul-domain/build.gradle.kts
+++ sulsul-domain/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-    // 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 }
 
 tasks.bootJar {
