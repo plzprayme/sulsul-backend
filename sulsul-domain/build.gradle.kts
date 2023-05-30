0b58fbb655b0ba3c8e3591139ddfeb72fdb069d1
plzprayme <dbfpzk142@gmail.com>
2023-05-31 08:40:01 +0900

    chore: set mongo db

--- sulsul-domain/build.gradle.kts
+++ sulsul-domain/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-    // 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//    runtimeOnly("com.h2database:h2")
+//    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 }
 
 tasks.bootJar {
