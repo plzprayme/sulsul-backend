674a117a9bb19fc5f52b153cc214e817750954c3
plzprayme <dbfpzk142@gmail.com>
2023-05-31 09:55:49 +0900

    feat: WIP jasypt

--- sulsul-domain/build.gradle.kts
+++ sulsul-domain/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
 //    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 //    runtimeOnly("com.h2database:h2")
 //    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
